d2e51d95a5d4118fd44b61957de7522f6591af57
LabW0lf <fenakral25@gmail.com>
2026-09-21 00:52:11 +0200

    early versions of final-stand finished

--- src/games/final-words/game.ts
+++ src/games/final-words/game.ts
@@ -2,25 +2,21 @@ import  { type Renderer } from '@engine/core/renderer.ts';
 import { config } from '@engine/config.ts';
 import { Entity } from '@engine/entity/entity.ts';
 import { Scene } from '@engine/scenes/scene.ts';
-import type { Input } from '@engine/core/input.ts';
+import { Input } from '@engine/core/input.ts';
 import { Game } from '@engine/core/game.ts';
 import { MenuScene } from '@engine/scenes/menuScene.ts';
 import type { AssetLoader } from '@engine/assets/assetloader.ts';
-import { ScoreDisplay } from '@engine/entity/scoreDisplay.ts';
 
 // ENTITIES
 class Border extends Entity {
 	// you loose life here
-	public static height: number = 5;
+	public static height: number = 148;
 	constructor(
 		x: number,
 		y: number,
 		w: number,
 		h: number) {
 		super(x, y, w, h);
-	}
-	update(dt: number) {
-
 	}
 
 	render(r: Renderer) {
@@ -79,22 +75,28 @@ class Difficulty extends Entity {
 		100, // easy & medium & hard words, easy & medium & hard math, country flags
 	];
 
-	current = 0;
+	static current = 0;
 	constructor(x: number, y: number, w: number, h: number) {
 		super(x, y, w, h);
 	}
 
 	update(dt: number) {
-		if (Level.lvl === this.atReachedLevel[this.current]) {
-			this.current += 1;
+		if (Level.lvl === this.atReachedLevel[Difficulty.current]) {
+			Difficulty.current += 1;
 		}
 	}
 
 	render(r: Renderer) {
-		r.advancedText(this.difficulty[this.current], this.x, this.y, config.theme.colors.dark_purple, {
-			textAlign: 'right',
-			textBaseline: 'middle',
-		});
+		r.advancedText(
+			this.difficulty[Difficulty.current],
+			this.x,
+			this.y,
+			config.theme.colors.dark_purple,
+			{
+				textAlign: 'right',
+				textBaseline: 'middle',
+			},
+		);
 	}
 }
 
@@ -121,69 +123,334 @@ export class Level extends Entity {
 	}
 }
 
-class Life extends Entity {
-	lives: number = 2;
-	streak: number = 10; // reach this number to earn a life
+export class Life extends Entity {
+	static lives: number = 2;
+	static streak: number = 0; // reach 10 to earn a life
 	constructor(x: number, y: number, w: number, h: number) {
 		super(x, y, w, h);
+
+
 	}
 
 	update(dt: number) {
+		if (Life.streak >= 10) {
+			Life.lives += 1;
+			Life.streak = 0;
+		}
 
+		if (Life.lives <= 0) {
+			GameScene.game_over();
+		}
 	}
 
 	render(r: Renderer) {
-		r.advancedText('HP: ' + String(this.lives), this.x, this.y, config.theme.colors.red, {
+		r.advancedText('HP: ' + String(Life.lives), this.x, this.y, config.theme.colors.red, {
 			textAlign: 'right',
 			textBaseline: 'middle',
 		});
 	}
 }
 
+export class PlayerInput extends Entity {
+	enableTyping = true;
+	static text = '';
+	input: Input = new Input();
+	color = config.theme.colors.black;
+
+	constructor(x: number, y: number, w: number, h: number) {
+		super(x, y, w, h);
+		this.input.onKeyDown((key) => {
+			if (this.enableTyping) {
+				if (key.length === 1 && PlayerInput.text.length < 25) {
+					PlayerInput.text += key.toLowerCase();
+				}
+				if (key === 'Backspace') {
+					PlayerInput.text = PlayerInput.text.slice(0, -1);
+				}
+				if (key === 'Enter') {
+					FallingWords.isHit(PlayerInput.text);
+					PlayerInput.text = '';
+				}
+			}
+		});
+	}
+
+	update(dt: number) {
+		if (PlayerInput.text.length >= 25) {
+			this.color = config.theme.colors.red;
+		} else {
+			this.color = config.theme.colors.black;
+		}
+	}
+
+	render(r: Renderer) {
+		r.advancedText(PlayerInput.text, this.x, this.y, this.color, {
+			textAlign: 'center',
+			textBaseline: 'middle',
+		});
+	}
+}
+
+export class FallingWords extends Entity {
+	text: string;
+	supercharged: boolean = false;
+	static universal_speed = 5;
+	speed: number = FallingWords.universal_speed;
+	static arena: string[] = [];
+
+	// ARRAYS
+	static easy_words: string[] = [
+		'cat',
+		'dog',
+		'sun',
+		'hat',
+		'book',
+		'tree',
+		'fish',
+		'house',
+		'car',
+		'milk',
+		'ball',
+		'star',
+		'door',
+		'rain',
+		'bird',
+		'hand',
+		'shoe',
+		'cake',
+		'moon',
+		'apple',
+	];
+
+	static medium_words: string[] = [
+		'garden',
+		'window',
+		'yellow',
+		'purple',
+		'friend',
+		'school',
+		'planet',
+		'summer',
+		'winter',
+		'forest',
+		'bridge',
+		'market',
+		'rabbit',
+		'coffee',
+		'pencil',
+		'flower',
+		'castle',
+		'button',
+		'bottle',
+		'rocket',
+	];
+
+	static hard_words: string[] = [
+		'adventure',
+		'beautiful',
+		'knowledge',
+		'important',
+		'challenge',
+		'excellent',
+		'mysterious',
+		'experience',
+		'necessary',
+		'different',
+		'algorithm',
+		'technology',
+		'environment',
+		'opportunity',
+		'extraordinary',
+		'responsibility',
+		'communication',
+		'imagination',
+		'determination',
+		'architecture',
+	];
+
+	constructor(x: number, y: number, w: number, h: number, text: string, supercharged = false) {
+		super(x, y, w, h);
+		this.text = text;
+		this.supercharged = supercharged;
+	}
+
+	static isHit(input: string) {
+		console.log('input: ' + input);
+		for (let i = 0; i < FallingWords.arena.length; i++) {
+			if (input === FallingWords.arena[i]) {
+				console.log('HIT: ' + FallingWords.arena[i]);
+				console.log('CURRENT ARENA: ' + FallingWords.arena);
+
+				for (let j = 0; j < GameScene.entities.length; j++) {
+					const entity = GameScene.entities[j];
+					if (entity instanceof FallingWords && entity.text === input) {
+						GameScene.entities.splice(j, 1);
+						break;
+					}
+				}
+
+				FallingWords.arena.splice(i, 1);
+				Life.streak += 1;
+				break;
+			}
+		}
+	}
+
+	timer = 0;
+	update(dt: number) {
+		this.y += this.speed * dt;
+
+		if (this.supercharged) {
+			this.color = config.theme.colors.yellow;
+			this.speed = FallingWords.universal_speed * 5;
+		}
+
+		if (this.y >= Border.height) {
+			console.log(this.text + 'HAS CROSSED THE BORDER!');
+
+			for (let i = 0; i < FallingWords.arena.length; i++) {
+				if (this.text === FallingWords.arena[i]) {
+					for (let j = 0; j < GameScene.entities.length; j++) {
+						const entity = GameScene.entities[j];
+						if (entity instanceof FallingWords && entity.text === this.text) {
+							GameScene.entities.splice(j, 1);
+							break;
+						}
+					}
+					FallingWords.arena.splice(i, 1);
+					break;
+				}
+			}
+
+			Life.lives -= 1;
+			Life.streak = 0;
+		}
+	}
+
+	color = config.theme.colors.white;
+	render(r: Renderer) {
+		r.advancedText(this.text, this.x, this.y, this.color, {
+			textAlign: 'center',
+		});
+	}
+}
+
+export class GameOver extends Entity {
+	constructor(x: number, y: number, w: number, h: number) {
+		super(x, y, w, h);
+	}
+
+	timer = 0;
+	isRed = false;
+	color = config.theme.colors.white;
+	update(delta: number) {
+
+		this.timer += delta;
+		if (this.timer >= 1) {
+			if (this.isRed) {
+				this.color = config.theme.colors.red;
+			} else {
+				this.color = config.theme.colors.white;
+			}
+			this.isRed = !this.isRed;
+			this.timer = 0;
+		}
+	}
+
+	render(r: Renderer) {
+		r.drawRect(0,0,240,180, config.theme.colors.dark_gray);
+		r.advancedText('GAME OVER', 120, 90, this.color, { textAlign: 'center' });
+	}
+}
 
 // GAME SCENE
 type GameState = 'start' | 'running' | 'end';
 
-class GameScene extends Scene {
-	public entities: Entity[] = [];
+export class GameScene extends Scene {
+	public static entities: Entity[] = [];
 
-	gamestate: GameState = 'running';
+	static gamestate: GameState = 'running';
 
 	constructor() {
 		super();
-
 		// created entities #######################################
-		this.entities.push(new Border(0,150, 240, 5));
-		this.entities.push(new Score(10,170,0,0));
-		this.entities.push(new Level(230, 170, 0, 0));
-		this.entities.push(new Life(120, 170, 0, 0));
-		this.entities.push(new Difficulty(230, 160, 0, 0));
+		GameScene.entities.push(new Border(0, 143, 240, 10));
+		GameScene.entities.push(new Score(10, 170, 0, 0));
+		GameScene.entities.push(new Level(230, 170, 0, 0));
+		GameScene.entities.push(new Life(config.canvas_width / 2, 170, 0, 0));
+		GameScene.entities.push(new Difficulty(230, 160, 0, 0));
+		GameScene.entities.push(new PlayerInput(config.canvas_width / 2, 149, 0, 0));
+	}
+
+	static supercharge_odds = 2;
+	addRandomFallingWord() {
+		const min = 15;
+		const max = 225;
+		const random_text = Math.floor(Math.random() * 20);
+		let random_x = Math.floor(Math.random() * (max - min + 1)) + min;
+		const supercharge_number = Math.floor(Math.random() * GameScene.supercharge_odds);
+
+		let text = 'text';
+		let supercharged = false;
+
+
+		if (Difficulty.current === 0) {
+			text = FallingWords.easy_words[random_text];
+		}
+
+
+		// if text is too far on the right
+		if ((random_x + text.length/2*7) > max) {
+			random_x = max - text.length/2*7;
+		}
+
+		// if text is too far on the left
+		if ((random_x - text.length/2*7) < min) {
+			random_x = min + text.length/2*7;
+		}
+
+		if (supercharge_number === 1) {
+			supercharged = true;
+		}
+
+		FallingWords.arena.push(text);
+		GameScene.entities.push(new FallingWords(random_x, -5, 0, 0, text, supercharged));
 	}
 
 	render(r: Renderer) {
-		for (const entity of this.entities) {
+		for (const entity of GameScene.entities) {
 			entity.render(r);
 		}
 	}
 
+	timer = 0;
+	spawn_per_second = 2;
 	update(dt: number, input: Input) {
-		if (this.gamestate === 'running') {
-			for (const entity of this.entities) {
+		if (GameScene.gamestate === 'running') {
+			for (const entity of GameScene.entities) {
 				entity.update(dt);
 			}
 		}
 
-		this.handleInput(input);
+		this.timer += dt;
+		if (this.timer >= this.spawn_per_second) {
+			this.addRandomFallingWord();
+			this.timer = 0;
+		}
 	}
 
-	handleInput(input: Input) {
-		// TODO: Handle keyboard/controller input here
-		// if (input.isDown(config.keys.confirm)) {
+	static game_over() {
+		this.gamestate = 'end';
+
+		GameScene.entities.push(new GameOver(0,0,0,0));
+
+		FinalStand.disable_reset = false;
 	}
 }
 
 // GAME
 export class FinalStand extends Game {
+	static disable_reset = false;
 	constructor() {
 		super();
 
@@ -193,7 +460,11 @@ export class FinalStand extends Game {
 	}
 
 	reset() {
-		this.scene = new GameScene();
+		// no resets, only startup
+		if (!FinalStand.disable_reset) {
+			this.scene = new GameScene();
+			FinalStand.disable_reset = true;
+		}
 	}
 
 	loadAssets(loader: AssetLoader) {
